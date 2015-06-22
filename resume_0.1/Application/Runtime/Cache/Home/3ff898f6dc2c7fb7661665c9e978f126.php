3098c6a8233b45ae0a1881dd9a786825541b9e82
xxlixin1993 <403136170@qq.com>
2015-06-22 23:17:56 +0800

    20150622

--- resume_0.1/Application/Runtime/Cache/Home/3ff898f6dc2c7fb7661665c9e978f126.php
+++
@@ -1,8 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit();?><h1>Blog Info</h1>
-
-
-<?php echo ($row["title"]); ?>
-<hr/>
-<?php echo ($row["content"]); ?>
-<hr/>
-<a href='/index.php/Home/Index/index'>返回首页</a>
